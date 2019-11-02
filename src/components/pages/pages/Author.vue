fd55f1a4501ad11d87438c681367cda9908651d2
romain <romain.quentel.bzh29@gmail.com>
2019-11-02 15:32:55 +0100

    Fix authors list

--- src/components/pages/pages/Author.vue
+++ src/components/pages/pages/Author.vue
@@ -5,7 +5,8 @@
             <AuthorPopup v-if="isPopupDisplayed" :author-id="authorPopupElementId"
                          v-on:popup-wanna-close="closePopup"></AuthorPopup>
 
-            <List ref="list" apiEndpoint="/api/authors" :cols="listCols" v-on:list-action-set="setAuthor"
+            <List ref="list" apiEndpoint="/api/authors" :cols="cols"
+                  v-on:list-action-set="setAuthor"
                   v-on:list-action-add="newAuthor"/>
 
         </template>
@@ -19,6 +20,7 @@
     import store from '../../../assets/js/store';
     import AuthorModule from '../../../assets/js/store/author';
     import AuthorPopup from "./author/AuthorPopup";
+    import Column from "../../../assets/ts/list/Column";
 
     if (!store.state['author']) {
         store.registerModule('author', AuthorModule);
@@ -35,10 +37,10 @@
             return {
                 isPopupDisplayed: false,
                 authorPopupElementId: null,
-                listCols: {
-                    'Nom': 'lastname',
-                    'Prénom': 'firstname',
-                }
+                cols: [
+                    new Column('lastname', 'Nom'),
+                    new Column('firstname', 'Prénom')
+                ]
             }
         },
         methods: {
